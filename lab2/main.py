28af5c6a800760db6156de0bb3a9d3ad931676a9
Andrei Iacob <andreiiacob15@gmail.com>
2022-10-11 17:54:05 +0300

    text refacto

--- lab2/main.py
+++ lab2/main.py
@@ -25,7 +25,8 @@ def ex2(list_of_numbers):
     return prime_numbers
 
 
-# Write a function that receives as parameters two lists a and b and returns: (an intersected with b, a reunited with b, a - b, b - a)
+# Write a function that receives as parameters two lists a and b and returns:
+# (an intersected with b, a reunited with b, a - b, b - a)
 def ex3(a, b):
     a = set(a)
     b = set(b)
@@ -40,14 +41,18 @@ def ex3(a, b):
     c.append(b - a)
 
 
-# Write a function that receives as a parameters a list of musical notes (strings), a list of moves (integers) and a start position (integer). The function will return the song composed by going through the musical notes beginning with the start position and following the moves given as parameter.	Example : compose(["do", "re", "mi", "fa", "sol"], [1, -3, 4, 2], 2) will return ["mi", "fa", "do", "sol", "re"]
+# Write a function that receives as a parameters a list of musical notes (strings), a list of moves (integers) and a
+# start position (integer). The function will return the song composed by going through the musical notes beginning
+# with the start position and following the moves given as parameter.
+# Example : compose(["do", "re", "mi", "fa", "sol"], [1, -3, 4, 2], 2) will return ["mi", "fa", "do", "sol", "re"]
 def ex4(notes, moves, start_position):
     print(notes[start_position], sep=" ")
     for move in moves:
         print(notes[move], sep=" ")
 
 
-# Write a function that receives as parameter a matrix and will return the matrix obtained by replacing all the elements under the main diagonal with 0 (zero).
+# Write a function that receives as parameter a matrix and will return the matrix obtained by replacing all the
+# elements under the main diagonal with 0 (zero).
 def ex5(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
@@ -58,8 +63,9 @@ def ex5(matrix):
     return matrix
 
 
-# Write a function that receives as a parameter a variable number of lists and a whole number x. Return a list containing the items that appear exactly x times in the incoming lists.
-# Example: For the [1,2,3], [2,3,4],[4,5,6], [4,1, "test"] and x = 2 lists [1,2,3 ] # 1 is in list 1 and 4, 2 is in list 1 and 2, 3 is in lists 1 and 2.
+# Write a function that receives as a parameter a variable number of lists and a whole number x. Return a list
+# containing the items that appear exactly x times in the incoming lists. Example: For the [1,2,3], [2,3,4],[4,5,6],
+# [4,1, "test"] and x = 2 lists [1,2,3 ] # 1 is in list 1 and 4, 2 is in list 1 and 2, 3 is in lists 1 and 2.
 def ex6(x, *args):
     count_app = dict()
     for a in args:
@@ -78,7 +84,9 @@ def ex6(x, *args):
     return items_with_occurrences_x
 
 
-# Write a function that receives as parameter a list of numbers (integers) and will return a tuple with 2 elements. The first element of the tuple will be the number of palindrome numbers found in the list and the second element will be the greatest palindrome number.
+# Write a function that receives as parameter a list of numbers (integers) and will return a tuple with 2 elements.
+# The first element of the tuple will be the number of palindrome numbers found in the list and the second element
+# will be the greatest palindrome number.
 def ex7(values):
     number_of_palindrome = 0
     greatest_palindrome = -1
@@ -93,8 +101,10 @@ def ex7(values):
     return number_of_palindrome, greatest_palindrome
 
 
-# Write a function that receives a number x, default value equal to 1, a list of strings, and a boolean flag set to True. For each string, generate a list containing the characters that have the ASCII code divisible by x if the flag is set to True, otherwise it should contain characters that have the ASCII code not divisible by x.
-#  Example: x = 2, ["test", "hello", "lab002"], flag = False will return (["e", "s"], ["e" . Note: The function must return list of lists.
+# Write a function that receives a number x, default value equal to 1, a list of strings, and a boolean flag set to
+# True. For each string, generate a list containing the characters that have the ASCII code divisible by x if the
+# flag is set to True, otherwise it should contain characters that have the ASCII code not divisible by x.
+# Example: x = 2, ["test", "hello", "lab002"], flag = False will return (["e", "s"], ["e"
 def ex8(strings, x=1, flag=True):
     returning_list = list()
     for text in strings:
@@ -109,14 +119,12 @@ def ex8(strings, x=1, flag=True):
     print(returning_list)
 
 
-# Write a function that receives as parameter a matrix which represents the heights of the spectators in a stadium and will return a list of tuples (line, column) each one representing a seat of a spectator which can't see the game. A spectator can't see the game if there is at least one taller spectator standing in front of him. All the seats are occupied. All the seats are at the same level. Row and column indexing starts from 0, beginning with the closest row from the field.
-# 	Example:
-# # FIELD
-# [[1, 2, 3, 2, 1, 1],
-# [2, 4, 4, 3, 7, 2],
-# [5, 5, 2, 5, 6, 4],
-# [6, 6, 7, 6, 7, 5]]
-# Will return : [(2, 2), (3, 4), (2, 4)]
+# Write a function that receives as parameter a matrix which represents the heights of the spectators in a stadium
+# and will return a list of tuples (line, column) each one representing a seat of a spectator which can't see the
+# game. A spectator can't see the game if there is at least one taller spectator standing in front of him. All the
+# seats are occupied. All the seats are at the same level. Row and column indexing starts from 0, beginning with the
+# closest row from the field. Example: # FIELD [[1, 2, 3, 2, 1, 1], [2, 4, 4, 3, 7, 2], [5, 5, 2, 5, 6, 4], [6, 6, 7,
+# 6, 7, 5]] Will return : [(2, 2), (3, 4), (2, 4)]
 def ex9(matrix):
     spectators = list()
     for i in range(0, len(matrix)):
@@ -131,9 +139,11 @@ def ex9(matrix):
     print(spectators)
 
 
-# Write a function that receives a variable number of lists and returns a list of tuples as follows: the first tuple contains the first items in the lists, the second element contains the items on the position 2 in the lists, etc.
-# Ex: for lists [1,2,3], [5,6,7], ["a", "b", "c"] return: [(1, 5, "a ") ,(2, 6, "b"), (3,7, "c")].
-# Note: If input lists do not have the same number of items, missing items will be replaced with None to be able to generate max ([len(x) for x in input_lists]) tuples.
+# Write a function that receives a variable number of lists and returns a list of tuples as follows: the first tuple
+# contains the first items in the lists, the second element contains the items on the position 2 in the lists,
+# etc. Ex: for lists [1,2,3], [5,6,7], ["a", "b", "c"] return: [(1, 5, "a ") ,(2, 6, "b"), (3,7, "c")]. Note: If
+# input lists do not have the same number of items, missing items will be replaced with None to be able to generate
+# max ([len(x) for x in input_lists]) tuples.
 def ex10(*args):
     max_length = max(len(a) for a in args)
     new_list = list()
@@ -159,9 +169,9 @@ def ex11(list_of):
     print(list_of)
 
 
-# Write a function that will receive a list of words  as parameter and will return a list of lists of words, grouped by rhyme. Two words rhyme if both of them end with the same 2 letters.
-# 	Example:
-#   group_by_rhyme(['ana', 'banana', 'carte', 'arme', 'parte']) will return [['ana', 'banana'], ['carte', 'parte'], ['arme']]
+# Write a function that will receive a list of words  as parameter and will return a list of lists of words,
+# grouped by rhyme. Two words rhyme if both of them end with the same 2 letters. Example: group_by_rhyme(['ana',
+# 'banana', 'carte', 'arme', 'parte']) will return [['ana', 'banana'], ['carte', 'parte'], ['arme']]
 def ex12(words):
     rhymes = list()
     for word in words:
@@ -184,7 +194,7 @@ if __name__ == '__main__':
 
     # ex3([1, 2, 3, 4, 5], [1, 2, 6, 8])
 
-    # ex4(["do", "re", "mi", "fa", "sol"], [1, -3, 4, 2], 2)
+    ex4(["do", "re", "mi", "fa", "sol"], [1, -3, 4, 2], 2)
 
     # ex5([[1, 1, 1, 1, 1, 1],
     #      [1, 1, 1, 1, 1, 1],
@@ -205,6 +215,6 @@ if __name__ == '__main__':
 
     # ex10([1,2,3], [5,6,7], ["a", "b", "c", "5"])
 
-    ex11([('abc', 'bcd'), ('abc', 'zza')])
+    # ex11([('abc', 'bcd'), ('abc', 'zza')])
 
     # ex12(['ana', 'banana', 'carte', 'arme', 'parte'])
